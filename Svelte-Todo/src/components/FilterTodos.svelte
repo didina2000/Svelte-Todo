123f34f1d25a7412c90343f6138627b148ca8377
Didina Vicliuc <didinacravcenco802@gmail.com>
2022-09-02 14:44:49 +0300

    filter todo items

---
+++ Svelte-Todo/src/components/FilterTodos.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { FiltersType } from "src/types/todo";
+
+  type SetFilterType = (newFilter: string) => void;
+
+  export let selectedFilter: FiltersType;
+  export let setFilter: SetFilterType;
+
+  let filters = ["all", "active", "completed"];
+</script>
+
+<div class="filters">
+  {#each filters as filter}
+    <button
+      on:click="{() => setFilter(filter)}"
+      class:selected="{selectedFilter === filter}"
+      class="filter"
+    >
+      {filter}
+    </button>
+  {/each}
+</div>
